7253ff987d955915a3f6019c392eff53132cd84a
leedanieluk <leedanieluk@gmail.com>
2025-02-22 11:51:00 +0000

    add clipboard mappings

--- lua/custom/mapping.lua
+++ lua/custom/mapping.lua
@@ -1,2 +1,7 @@
 vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = 'Open Explorer' })
 
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>p', '"+p')
+
