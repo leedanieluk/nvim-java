48e16cbb53cf7bf7fe05e73660debbeaf17261f0
leedanieluk <leedanieluk@gmail.com>
2025-02-24 09:55:26 +0000

    add mappings and options

--- lua/custom/mapping.lua
+++ lua/custom/mapping.lua
@@ -1,7 +1,8 @@
+-- view
 vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = 'Open Explorer' })
 
+-- editor
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>p', '"+p')
 vim.keymap.set('v', '<leader>p', '"+p')
-
